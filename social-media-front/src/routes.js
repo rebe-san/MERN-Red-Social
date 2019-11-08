6988bc6d83bb7707070dec50802a033e6115e616
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-11-08 17:32:41 -0600

    Modifying Router React

--- social-media-front/src/routes.js
+++ social-media-front/src/routes.js
@@ -3,7 +3,7 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-  } from "react-router-dom";
+} from "react-router-dom";
 
 // Views
 
@@ -16,20 +16,18 @@ import Profile from './views/Profile';
 
 //Component
 class App extends Component {
-
-
     render() {
-        return ( 
-        <Router basename = { window.location.pathname || '' } >
-            <Switch >
-
-            <Route exact path = '/' component = { Login }/>
-            <Route exact path = '/feed' component = { Feed }/>
-            <Route exact path = '/about-us' component = { AboutUs }/>
-            <Route exact path = '/profile' component = { Profile } />
-
-            </Switch> 
-        </Router >
+        console.log(window.location.pathname);
+        return (
+            <Router>
+                {/* <Router basename = { window.location.pathname || '' } > */}
+                <Switch >
+                    <Route exact path='/' component={Login} />
+                    <Route exact path='/feed' component={Feed} />
+                    <Route exact path='/about-us' component={AboutUs} />
+                    <Route exact path='/profile' component={Profile} />
+                </Switch>
+            </Router >
         )
     };
 }
